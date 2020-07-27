e324a81dae835c77f53302a16a8719bbf86fd130
nextgtrgod <KYakovlev7@gmail.com>
2020-07-27 19:41:37 +0300

    deploy

--- build/01/modules/Sketch/index.js
+++ build/01/modules/Sketch/index.js
@@ -1,9 +1,13 @@
 import rnd from "../../utils/random.js";
 import Dot2 from "./Dot.js";
+let dpi = window.devicePixelRatio;
+let W = window.innerWidth * dpi;
+let H = window.innerHeight * dpi;
+let threshold = Math.max(W, H) / 3;
+const PI = Math.PI;
 class Sketch {
   constructor(canvas) {
     this.canvas = canvas;
-    this.dpi = window.devicePixelRatio;
     this.radId = null;
     this.init();
     let resizeTimer = null;
@@ -16,10 +20,11 @@ class Sketch {
     });
   }
   init() {
-    this.W = window.innerWidth * this.dpi;
-    this.H = window.innerHeight * this.dpi;
-    this.canvas.width = this.W;
-    this.canvas.height = this.H;
+    W = window.innerWidth * dpi;
+    H = window.innerHeight * dpi;
+    threshold = Math.max(W, H) / 3;
+    this.canvas.width = W;
+    this.canvas.height = H;
     this.ctx = this.canvas.getContext("2d", {
       alpha: false
     });
@@ -29,18 +34,16 @@ class Sketch {
     this.dots = [];
     let count = 16;
     let speed = 2;
-    this.threshold = this.W / 3;
-    const PI = Math.PI;
     let scale = window.innerWidth >= 600 ? 2 : 1;
     for (let i = 0; i < count; i++) {
-      let s = rnd.range(0.5, speed) * this.dpi;
+      let s = rnd.range(0.5, speed) * dpi;
       let limit = PI / 12;
       let angle = rnd.from([rnd.range(limit, PI / 2 - limit), rnd.range(PI / 2 + limit, PI - limit), rnd.range(PI + limit, 1.5 * PI - limit), rnd.range(1.5 * PI + limit, 2 * PI - limit)]);
       this.dots.push(new Dot2({
         id: i,
-        x: rnd.range(0, this.W),
-        y: rnd.range(0, this.H),
-        r: rnd.range(3, 5) * scale * this.dpi,
+        x: rnd.range(0, W),
+        y: rnd.range(0, H),
+        r: rnd.range(3, 5) * scale * dpi,
         v: {
           x: s * Math.cos(angle),
           y: s * Math.sin(angle)
@@ -50,9 +53,9 @@ class Sketch {
   }
   draw() {
     this.ctx.fillStyle = "#000";
-    this.ctx.fillRect(0, 0, this.W, this.H);
+    this.ctx.fillRect(0, 0, W, H);
     for (let i = 0; i < this.dots.length; i++) {
-      this.dots[i].update(this.ctx, this.W, this.H, this.dots, this.threshold);
+      this.dots[i].update(this.ctx, W, H, this.dots, threshold);
       for (let id in this.dots[i].lines) {
         this.ctx.beginPath();
         this.ctx.moveTo(this.dots[i].lines[id][0].x, this.dots[i].lines[id][0].y);
